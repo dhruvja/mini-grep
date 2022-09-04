ea37d240fcf584c1122dc4a9bbcbd164130e79c5
dhruvja <dhruv@iamsizzling.com>
2022-09-04 14:31:48 +0530

    replaced loops with iterators

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,7 @@ use std::process;
 use minigrep::{Config, run};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config: Config = Config::build(&args).unwrap_or_else(|err| {
+    let config: Config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("The progam failed with {}", err);
         process::exit(1);
     });
